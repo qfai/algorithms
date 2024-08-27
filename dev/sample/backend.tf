4d64b6f8588251e8877d8cb6d308cf1bf39781e5
qfai <404630507@qq.com>
2024-08-27 16:36:20 +0800

    Create files

---
+++ dev/sample/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "jundachen"
+    storage_account_name = "arcsctest"
+    container_name       = "esa"
+    key                  = "sample.tfstate"
+    use_azuread_auth     = true
+    subscription_id      = "b9e38f20-7c9c-4497-a25d-1a0c5eef2108"
+  }
+}
